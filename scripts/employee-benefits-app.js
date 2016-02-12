f91868aaaac23c1ed689ef2c85c46dcaa17e1396
DJ Watson <david.watsonjr@gmail.com>
2016-02-12 15:04:33 -0500

    Added more details to the Employer Benefits app

--- scripts/employee-benefits-app.js
+++ scripts/employee-benefits-app.js
@@ -1,20 +1,37 @@
 var EmployeeBenefitsApp = angular.module('EmployeeBenefitsApp', []);
 
-function Employee(){
-	this.firstName = null;
-	this.lastName = null;
-	this.id = null;
-	this.editing = true;
-	this.dependants = [];
-	this.salary = 2000;
+function Employee(settings){
+	settings = settings || {};
+
+	this.firstName = settings.firstName || null;
+	this.lastName = settings.lastName || null;
+	this.id = settings.id || null;
+	this.editing = this.id === null;
+	this.dependants = settings.dependants || [];
+	this.salary = settings.salary || 2000;
+
+	var baseBenefitsCost = settings.baseBenefitsCost || 1000;
+	
+	Object.defineProperty(this, "annualSalary", { get: function () { return this.salary * 26; } });
+	Object.defineProperty(this, "benefitsCost", { get: function () {
+		return (this.firstName !== null && this.firstName.toLowerCase().substr(0,1) === 'a')? baseBenefitsCost * 0.9 : baseBenefitsCost;
+	}});
 };
 
-function Dependant(){
-	this.firstName = null;
-	this.lastName = null;
-	this.id = null;
-	this.editing = true;
-	this.relation = null;
+function Dependant(settings){
+	settings = settings || {};
+
+	this.firstName = settings.firstName || null;
+	this.lastName = settings.lastName || null;
+	this.id = settings.id || null;
+	this.editing = this.id === null;
+	this.relation = settings.relation || null;
+
+	var baseBenefitsCost = settings.baseBenefitsCost || 500;
+	
+	Object.defineProperty(this, "benefitsCost", { get: function () {
+		return (this.firstName !== null && this.firstName.toLowerCase().substr(0,1) === 'a')? baseBenefitsCost * 0.9 : baseBenefitsCost;
+	}});
 };
 
 EmployeeBenefitsApp.controller('EmployeeListCtrl', ['$scope',
@@ -43,7 +60,7 @@ EmployeeBenefitsApp.controller('EmployerCostsCtrl', ['$scope',
 
 			for(var i = 0; i < $scope.employeeCollection.length; i++){
 				if($scope.employeeCollection[i].id !== null){
-					total += $scope.employeeCollection[i].salary * paychecksPerYear;
+					total += $scope.employeeCollection[i].annualSalary;
 				}
 			}
 
@@ -60,14 +77,12 @@ EmployeeBenefitsApp.controller('EmployerCostsCtrl', ['$scope',
 				var employee = $scope.employeeCollection[i];
 
 				if(employee.id !== null){
-					total += (employee.firstName.toLowerCase().substr(0,1) === 'a')? employeeCost * (1 - discountForAName) : employeeCost;
+					total += employee.benefitsCost;
 				}
-
-				console.log(employee.dependants);
 
 				for(var t = 0; t < employee.dependants.length; t++){
 					if(employee.dependants[t].id !== null){
-						total += (employee.dependants[t].firstName.toLowerCase().substr(0,1) === 'a')? dependantCost * (1 - discountForAName) : dependantCost;
+						total += employee.dependants[t].benefitsCost;
 					}
 				}
 			}
